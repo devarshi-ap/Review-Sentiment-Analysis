2fa13cdc69e0d6b120e6d666b0836992dc5ee0ac
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-11-10 09:57:08 -0500

    Create Main.java

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+class Main{
+        public static void main(String[] args){
+                
+                // show file in action
+                // total sentiment value of sample review txt file
+                // star rating of restaurant review file
+                // Negative slanted version of sample review txt file
+                // Positive slanted version of sample review txt file
+                
+        
+        }
+}
