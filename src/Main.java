d8bed9b80b34f938e9be27275d1236a996c973f0
Dev Patel <59234436+devarshi-ap@users.noreply.github.com>
2020-11-11 14:38:36 -0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,16 @@ class Main{
       public static void main(String[] args){
             
             // show file and it's content
-            System.out.println("File: \"SampleReview.txt\"");
-            System.out.printf("Content:%n%s", Review.fileToString("SampleReview.txt"));
+            System.out.printf("File  ==  \"SampleReview.txt\"%n%n");
+            System.out.printf("File Content:%n%s", Review.fileToString("SampleReview.txt"));
             
             // total sentiment value of file
-            System.out.printf("%n%nTotal Sentiment Value of \"SampleReview.txt\" :  %f%n", Review.totalSentiment("SampleReview.txt"));
+            System.out.printf("%n%n%nTotal Sentiment Value of \"SampleReview.txt\"  ==  %.2f", Review.totalSentiment("SampleReview.txt"));
             
             // Negative slanted version of file
-            System.out.printf("%nA more NEGATIVE version of \"SampleReview.txt\" :%n%s", Review.slantMoreNegative("SampleReview.txt"));
+            System.out.printf("%n%n%nA more NEGATIVE version of \"SampleReview.txt\" :%n%s", Review.slantMoreNegative("SampleReview.txt"));
                                
             // Positive slanted version of file
-            System.out.printf("%nA more POSITIVE version of \"SampleReview.txt\" :%n%s", Review.slantMorePositive("SampleReview.txt"));
+            System.out.printf("%n%n%nA more POSITIVE version of \"SampleReview.txt\" :%n%s", Review.slantMorePositive("SampleReview.txt"));
       }
 }
